93a0ea35365e0c9b340970c9d4ab30493fd424c9
Muuyo <>
2025-04-09 12:44:04 +0800

    feat(block_ctrl_w_close_pin_tab): v2

--- js/block_ctrl_w_close_pin_tab.js
+++ js/block_ctrl_w_close_pin_tab.js
@@ -1,5 +1,12 @@
-// version 1
-(()=>{
+// 之前的实现方式是永久性修改，确实会产生一些副作用，毕竟思源挺多功能还是依赖这个 item-focus 的。
+// 所以，我改了实现方式，现在通过按键临时修改 item-focus，按键结束就恢复。这样就不易产生副作用。
+// 并且，增加了，在切换标签对话框中，钉住的标签页也置顶的功能，如果使用把 `isMovePinTabToTopInTabSwitchDialog` 设为 true 即可，默认已经设为 true 了，如果不使用设为 false 即可。
+// 复制下面的代码，并替换原来的代码即可。
+// 别忘了样式的备份与更新。
+// 但，这样实现方式也有个缺点，这个与快捷键绑定，当你修改了关闭标签的快捷键时需要修改代码，不过快捷键一般不会修改，相信仅仅修改快捷键代码你应该还是会的。
+// version 2
+// see https://ld246.com/article/1723109908986
+(() => {
   // 注入样式，模拟激活标签的样式，可根据自己的样式进行调整
   addStyle(`
         .layout__wnd--active .layout-tab-bar .item--pin--focus:after {
@@ -16,87 +23,120 @@
         }
     `);
 
-  // 是否在最近打开文档中，将钉住标签移动到最上面，默认不移动，改为true时则移动
-  const isMovePinTabToTopInRecentlyDialog  = true;
+  // 是否在最近打开文档对话框中，将钉住标签移动到最上面，默认不移动，改为true时则移动
+  const isMovePinTabToTopInRecentlyDialog = true;
+
+  // 是否在标签切换对话框中，将钉住标签移动到最上面，默认不移动，改为true时则移动
+  const isMovePinTabToTopInTabSwitchDialog = true;
 
   //////////////// 以下代码，不涉及样式调整，非必要勿动 //////////////////////
 
   // 等待标签页容器渲染完成后开始监听
   whenElementExist('.layout__center').then(async element => {
-    // 激活焦点文档，监控前触发一次
-    focusActiveTab();
+    // 记录按键前的数据状态
+    let originalValues = {};
 
-    // 监听页签切换事件
-    let stopObserving;
-    const onTabChange = (tab) => {
-      // 暂停监听，防止修改class属性不停回调引起死循环
-      stopObserving();
-      console.log(tab, "tab");
-      // 如果是钉住标签，则取消焦点样式（取消后ctrl+w无法关闭），使用pin焦点样式代替
-      if(tab.classList.contains("item--pin")) {
-        clearPinFocus();
-        tab.classList.remove("item--focus");
-        tab.classList.add("item--pin--focus");
-      } else {
-        // 如果不是钉住标签分两种情况，一是取消了标签，二是切换到其他标签
-        if(tab.classList.contains("item--pin--focus")) {
-          // 如果是取消钉住，则还原原焦点样式
-          tab.classList.remove("item--pin--focus");
-          tab.classList.add("item--focus");
+    // 当按ctrl+w时，临时修改item--focus为item--pin--focus
+    document.addEventListener('keydown', function (event) {
+      if ((event.ctrlKey || event.metaKey) && event.key === 'w') {
+        // 临时修改item--focus为item--pin--focus
+        const focusPinTab = element.querySelector('.layout-tab-bar .item--pin.item--focus');
+        if (focusPinTab) {
+          focusPinTab.classList.remove('item--focus');
+          focusPinTab.classList.add('item--pin--focus');
+        }
 
-        } else {
-          // 切换到其他标签时，清除现在的钉住标签样式即可
-          clearPinFocus();
+        // 临时修改activetime为0，值为0可以避免被获得焦点
+        if (Object.keys(originalValues).length === 0) {
+          const items = element.querySelectorAll('.layout-tab-bar .item--pin');
+          let maxActivetimeItem = null;
+          let maxActivetime = 0;
+          items.forEach(item => {
+            originalValues[item.getAttribute("data-id")] = item.dataset.activetime;
+            if (item.dataset.activetime > maxActivetime) {
+              maxActivetime = item.dataset.activetime;
+              maxActivetimeItem = item;
+            }
+            item.dataset.activetime = 0;
+          });
+          // 把最后激活的文档设为下一个焦点
+          if (maxActivetimeItem) maxActivetimeItem.dataset.activetime = 1;
         }
       }
-      // 重新拉起监听
-      stopObserving = observeTabChanged(element, onTabChange);
-    };
-    // 开始监听
-    stopObserving = observeTabChanged(element, onTabChange);
+    }, {capture: true});
 
-    // 激活焦点文档，监控后触发一次
-    focusActiveTab();
+    // 当按释放按键是，恢复item--pin--focus为item--focus
+    document.addEventListener('keyup', function (event) {
+      // 恢复item--pin--focus为item--focus
+      const focusPinTab = element.querySelector('.layout-tab-bar .item--pin.item--pin--focus');
+      if (focusPinTab) {
+        focusPinTab.classList.remove('item--pin--focus');
+        focusPinTab.classList.add('item--focus');
+      }
 
-    // 当按ctrl+w时，把钉住标签的激活时间临时改为0，防止连续按键时，钉住标签被切换成焦点的情况发生
-    modifyActiveTimeOnShortcut(".layout__center .layout-tab-bar li.item--pin", 0);
+      // 恢复activetime的值
+      if (Object.keys(originalValues).length > 0) {
+        const items = element.querySelectorAll('.layout-tab-bar .item--pin');
+        items.forEach(item => {
+          if (item.dataset.activetime <= 1) item.dataset.activetime = originalValues[item.getAttribute("data-id")];
+        });
+        //清空originalValues
+        Object.keys(originalValues).forEach(key => {
+          delete originalValues[key];
+        });
+      }
+    }, true);
 
-    // 监听最近打开文档
-    if(isMovePinTabToTopInRecentlyDialog) {
-      observeRecentlyDialog((node)=>{
-        // 获取所有钉住的标签
-        const pinTabNodeIds = getAllPinTabNodeIds();
-        // 把所有最近文档的钉住标签，移动到顶部
-        movePinTabItemToTop(node, pinTabNodeIds);
+    // 监听最近打开文档 和 页签切换
+    if (isMovePinTabToTopInRecentlyDialog || isMovePinTabToTopInTabSwitchDialog) {
+      observeDialogShow((node, dialog) => {
+        // 监听最近打开文档
+        if (dialog === 'dialog-recentdocs') {
+          // 获取所有钉住的标签
+          const pinTabNodeIds = getAllPinTabNodeIds();
+          // 把所有最近文档的钉住标签，移动到顶部
+          movePinTabItemToTop(node, pinTabNodeIds);
+        }
+        // 监听页签切换
+        if (dialog === 'dialog-switchtab') {
+          // 获取所有钉住的标签
+          const pinTabNodeIds = getAllPinTabNodeIds();
+          // 把所有最近文档的钉住标签，移动到顶部
+          movePinTabItemToTop(node, pinTabNodeIds);
+        }
       });
     }
 
   });
-
-  // 激活焦点文档
-  function focusActiveTab() {
-    // 激活焦点文档
-    document.querySelector(".layout__center .layout-tab-bar li.item--focus")?.click();
-  }
 
   // 获取所有的指定标签的nodeId
   function getAllPinTabNodeIds() {
     const pinTabs = document.querySelectorAll(".layout__center .layout-tab-bar li.item--pin");
     let pinTabNodeIds = [];
     pinTabs.forEach(pin => {
-      const dataId = pin.getAttribute("data-id");
-      const parents = pin.closest("div.fn__flex");
-      const nodeId = parents?.nextElementSibling?.querySelector('div[data-id="'+dataId+'"] .protyle-breadcrumb__item svg.popover__block')?.getAttribute("data-id");
-      if(nodeId) {
-        pinTabNodeIds.push(nodeId);
+      let initData = pin.getAttribute("data-initdata");
+      if (initData) {
+        // 初始化时的数据
+        initData = JSON.parse(initData);
+        if (initData && initData.blockId) {
+          pinTabNodeIds.push(initData.blockId);
+        }
+      } else {
+        // 内容加载后的数据
+        const dataId = pin.getAttribute("data-id");
+        const parents = pin.closest("div.fn__flex");
+        const nodeId = parents?.nextElementSibling?.querySelector('div[data-id="' + dataId + '"] .protyle-breadcrumb__item svg.popover__block')?.getAttribute("data-id");
+        if (nodeId) {
+          pinTabNodeIds.push(nodeId);
+        }
       }
     });
     return pinTabNodeIds;
   }
 
-  // 移动最近打开文档列表的node-id在pinTabNodeIds中的元素置顶
+  // 移动最近打开文档或文档切换列表的node-id在pinTabNodeIds中的元素置顶
   async function movePinTabItemToTop(node, pinTabNodeIds) {
-    const ulElement = await whenElementExist(()=>{
+    const ulElement = await whenElementExist(() => {
       return node.querySelector("ul.b3-list--background.fn__flex-1");
     });
     pinTabNodeIds.reverse();
@@ -111,92 +151,8 @@
     });
   }
 
-  // 清除上次的pin焦点
-  function clearPinFocus() {
-    document.querySelector(".item--pin--focus")?.classList.remove("item--pin--focus");
-  }
-
-  // 当按ctrl+w时，把钉住标签的激活时间临时改为0，防止连续按键时，钉住标签被切换成焦点的情况发生
-  function modifyActiveTimeOnShortcut(selector, tempValue) {
-    const elements = document.querySelectorAll(selector);
-    let isCtrlOrCmdPressed = false;
-    let isWPressed = false;
-    let originalValues = {};
-    // 添加 keydown 事件监听器
-    document.addEventListener('keydown', function(event) {
-      if ((event.key === 'Control' || event.metaKey) && !isCtrlOrCmdPressed) {
-        isCtrlOrCmdPressed = true;
-      }
-      if (event.key === 'w' && isCtrlOrCmdPressed) {
-        isWPressed = true;
-        // 阻止默认行为
-        event.preventDefault();
-        // 改变所有元素的 data-activetime 属性
-        elements.forEach(function(element) {
-          originalValues[element] = element.dataset.activetime;
-          element.dataset.activetime = tempValue;
-        });
-      }
-    });
-    // 添加 keyup 事件监听器
-    document.addEventListener('keyup', function(event) {
-      if (event.key === 'Control' || event.metaKey) {
-        isCtrlOrCmdPressed = false;
-      }
-      if (event.key === 'w') {
-        isWPressed = false;
-        // 检查是否两个键都已释放
-        if (!isCtrlOrCmdPressed && !isWPressed) {
-          // 恢复所有元素的原始 data-activetime 属性
-          elements.forEach(function(element) {
-            if (originalValues[element]) {
-              element.dataset.activetime = originalValues[element];
-            }
-          });
-        }
-      }
-    });
-  }
-
-  // 监听页签切换事件
-  function observeTabChanged(parentNode, callback) {
-    // 创建一个回调函数来处理观察到的变化
-    const observerCallback = function(mutationsList, observer) {
-      // 用常规方式遍历 mutationsList 中的每一个 mutation
-      for (let mutation of mutationsList) {
-        // 属性被修改
-        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-          const element = mutation.target;
-          if (element.tagName.toLowerCase() === 'li' && element.getAttribute('data-type') === 'tab-header' && (element.classList.contains('item--focus')||element.classList.contains('item--pin--focus'))) {
-            if(typeof callback === 'function') callback(element);
-          }
-        }
-        // 如果有新的子节点被添加
-        if (mutation.type === 'childList') {
-          mutation.addedNodes.forEach(node => {
-            if (node.nodeType === Node.ELEMENT_NODE && node.tagName.toLowerCase() === 'li') {
-              if (node.getAttribute('data-type') === 'tab-header' && (node.classList.contains('item--focus')||node.classList.contains('item--pin--focus'))) {
-                if(typeof callback === 'function') callback(node);
-              }
-            }
-          });
-        }
-      }
-    };
-    // 创建一个观察器实例并传入回调函数
-    const observer = new MutationObserver(observerCallback);
-    // 配置观察器：传递一个对象来指定观察器的行为
-    const config = { attributes: true, attributeFilter: ['class'], childList: true, subtree: true };
-    // 开始观察目标节点
-    observer.observe(parentNode, config);
-    // 返回一个函数，用于停止观察
-    return function stopObserving() {
-      observer.disconnect();
-    };
-  }
-
-  // 监听最近打开的文档
-  function observeRecentlyDialog(callback) {
+  // 监听对话框
+  function observeDialogShow(callback) {
     // 创建 MutationObserver 实例
     const observer = new MutationObserver((mutationsList) => {
       for (let mutation of mutationsList) {
@@ -205,7 +161,10 @@
           for (let node of mutation.addedNodes) {
             if (node.nodeType === Node.ELEMENT_NODE && node.tagName.toLowerCase() === 'div') {
               if (node.hasAttribute('data-key') && node.getAttribute('data-key') === 'dialog-recentdocs') {
-                if(typeof callback === 'function') callback(node);
+                if (typeof callback === 'function') callback(node, 'dialog-recentdocs');
+              }
+              if (node.hasAttribute('data-key') && node.getAttribute('data-key') === 'dialog-switchtab') {
+                if (typeof callback === 'function') callback(node, 'dialog-switchtab');
               }
             }
           }
